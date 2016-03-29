cabd418e9e301958033975f010c35cd1f9e6ff4b
lmv1992 <l.mendozav@alumnos.urjc.es>
2016-03-29 19:18:57 +0200

    codigo d django

--- calc/views.py
+++ calc/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def sumar(request, num1, num2):
+	suma = int(num1) + int(num2)
+	return HttpResponse("<b><h1>La suma de "
+						+ str(num1) + " + "
+						+ str(num2) + " es "
+						+ '<font color="blue">' + str(suma) + '</font>'
+						+"</h1></b>")
+						
+def restar(request, num1, num2):
+	resta = int(num1) - int(num2)
+	return HttpResponse("<b><h1>La suma de "
+						+ str(num1) + " - "
+						+ str(num2) + " es "
+						+ '<font color="blue">' + str(resta) + '</font>'
+						+"</h1></b>")
+
+def multiplicar(request, num1, num2):
+	multiplicacion = int(num1) * int(num2)
+	return HttpResponse("<b><h1>La multiplicacion de "
+						+ str(num1) + " * "
+						+ str(num2) + " es "
+						+ '<font color="blue">' + str(multiplicacion) + '</font>'
+						+"</h1></b>")
+
+def dividir(request, num1, num2):
+	division = int(num1) / int(num2)
+	return HttpResponse("<b><h1>La division de "
+						+ str(num1) + " / "
+						+ str(num2) + " es "
+						+ '<font color="blue">' + str(division) + '</font>'
+						+"</h1></b>")
+
+def error(request):
+	return HttpResponse('<b><h1><font color="yellow">Pagina no encontrada</font></h1></b>')
--- project/urls.py
+++ project/urls.py
@@ -2,9 +2,13 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+	# Examples:
+	# url(r'^$', 'project.views.home', name='home'),
+	# url(r'^blog/', include('blog.urls')),
+	url(r'(\d+)\+(\d+)$', "calc.views.sumar"),
+	url(r'(\d+)-(\d+)$', "calc.views.restar"),
+	url(r'(\d+)\*(\d+)$', "calc.views.multiplicar"),
+	url(r'(\d+)/(\d+)$', "calc.views.dividir"),
+	url(r'^admin/', include(admin.site.urls)),
+	url(r'.*', "calc.views.error"),
 )
